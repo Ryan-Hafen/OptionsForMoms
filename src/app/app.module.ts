666794da9296500b415c252bec510b1c5b0f8e31
Ryan Hafen <ryan.hafen@icloud.com>
2021-02-05 15:39:10 -0800

    add ng-boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './nav/navbar/navbar.component';
+import { SidebarComponent } from './nav/sidebar/sidebar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
       NavbarComponent,
       SidebarComponent
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
